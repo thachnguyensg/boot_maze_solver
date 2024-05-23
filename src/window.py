1fee6ace6bb93eeac797276b669cfd091a2727b6
Thach Nguyen <thachnn.sg@gmail.com>
2024-05-23 22:29:06 +0700

    feat: simple window

--- src/window.py
+++ src/window.py
@@ -9,7 +9,8 @@ class Window:
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas = Canvas(self.__root, width=width, height=height, bg="white")
+        self.canvas.pack(fill=BOTH, expand=True)
         self.running = False
 
     def redraw(self):
@@ -17,10 +18,13 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
+        print("Waiting for window to close")
         self.running = True
         self.redraw()
+        self.__root.mainloop()
 
     def close(self):
+        print("Closing window")
         self.running = False
         self.__root.destroy()
 
